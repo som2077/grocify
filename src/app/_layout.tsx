11aa75ace4422a08b2cb89ac5abf71098073c3c3
som2077 <somgautam2077@gmail.com>
2026-04-21 14:20:55 +0530

    feat(auth): implement social authentication with Google, GitHub, and Apple chore: add environment configuration and update Tailwind CSS theme style: enhance sign-in screen layout and add images

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,10 @@
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import "../../global.css";
+import { useColorScheme } from "react-native";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
@@ -11,13 +13,17 @@ if (!publishableKey) {
 }
 
 export default function RootLayout() {
+
+  const colorSchema = useColorScheme()
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <ThemeProvider value={colorSchema === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
