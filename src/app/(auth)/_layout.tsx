ad0ff85aba11047db05f46ffd51fb9a53c3d4286
som2077 <somgautam2077@gmail.com>
2026-04-22 09:07:20 +0530

    feat(api): add CRUD operations for grocery items and clear purchased items

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,18 +1,22 @@
-import { useAuth } from '@clerk/expo'
-import { Redirect, Stack } from 'expo-router'
+import { useAuth } from "@clerk/expo";
+import { Redirect, Stack } from "expo-router";
 
 export default function AuthRoutesLayout() {
-  const { isSignedIn, isLoaded } = useAuth()
+  const { isSignedIn, isLoaded } = useAuth();
 
   if (!isLoaded) {
-    return null
+    return null;
   }
 
   if (isSignedIn) {
-    return <Redirect href={'/'} />
+    return <Redirect href={"/"} />;
   }
 
-  return <Stack screenOptions={{
+  return (
+    <Stack
+      screenOptions={{
         headerShown: false,
-      }}  />
+      }}
+    />
+  );
 }
