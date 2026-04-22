af1e708471370a272689cc1bd7f0714d2332305d
som2077 <somgautam2077@gmail.com>
2026-04-22 09:19:54 +0530

    feat(store): implement grocery store state management with Zustand feat(tabs): enhance tab layout with item loading functionality feat(auth): add SSO callback screen for authentication flow

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,13 +1,22 @@
+import { useGroceryStore } from "@/store/grocery-store";
 import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 import { NativeTabs } from "expo-router/unstable-native-tabs";
 import { useColorScheme } from "nativewind";
+import { useEffect } from "react";
 
 export default function TabsLayout() {
   const { isSignedIn, isLoaded } = useAuth();
+
+  const { loadItems, items } = useGroceryStore();
+
   const { colorScheme } = useColorScheme();
   const isDark = colorScheme === "dark";
   const tabTintColor = isDark ? "hsl(142 70% 54%)" : "hsl(147 75% 33%)";
+
+  useEffect(() => {
+    loadItems();
+  }, []);
 
   if (!isLoaded) {
     return null;
@@ -20,15 +29,29 @@ export default function TabsLayout() {
   return (
     <NativeTabs tintColor={tabTintColor}>
       <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>Lists</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon sf="house.fill" md="list" />
+        <NativeTabs.Trigger.Label>List</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          sf={{
+            default: "list.bullet.clipboard",
+            selected: "list.bullet.clipboard.fill",
+          }}
+          md="list"
+        />
       </NativeTabs.Trigger>
+
       <NativeTabs.Trigger name="planner">
-        <NativeTabs.Trigger.Icon sf="plus.circle" md="add" />
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "plus.circle", selected: "plus.circle.fill" }}
+          md="add"
+        />
         <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
+
       <NativeTabs.Trigger name="insights">
-        <NativeTabs.Trigger.Icon sf="plus.circle" md="analytics" />
+        <NativeTabs.Trigger.Icon
+          sf={{ default: "chart.bar", selected: "chart.bar.fill" }}
+          md="analytics"
+        />
         <NativeTabs.Trigger.Label>Insights</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
